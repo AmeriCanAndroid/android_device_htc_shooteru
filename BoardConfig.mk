1cbe99b0f415a859e8150defbc590a07d137c38c
Fraser <fraser@Media-VirtualBox.(none)>
2013-01-20 22:03:03 -0700

    board uses new htc ril as per warren & other: cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,15 +24,14 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
+USE_CAMERA_STUB := true
+
 # inherit from common msm8660
 -include device/htc/msm8660-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/htc/shooteru/BoardConfigVendor.mk
 
-TARGET_SPECIFIC_HEADER_PATH += device/htc/shooteru/include
-
-BOARD_VENDOR := htc
 TARGET_BOOTLOADER_BOARD_NAME := shooteru
 
 # Kernel
@@ -46,7 +45,29 @@ BOARD_KERNEL_PAGE_SIZE := 2048
 # Not Building a kernel at this time
 TARGET_PREBUILT_KERNEL := device/htc/shooteru/prebuilt/zImage
 
-# Partitions
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := shooteru
+
+# 3D Support
+TARGET_HARDWARE_3D := true
+
+# Camera
+TARGET_DISABLE_ARM_PIE := true
+BOARD_CAMERA_USE_MM_HEAP := true
+BOARD_HTC_3D_SUPPORT := true
+
+# -DQCOM_TUNNEL_LPA_ENABLED 
+TARGET_USES_QCOM_LPA := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_LPA_COMPAT
+COMMON_GLOBAL_CFLAGS += -DQCOM_ACDB_ENABLED -DQCOM_VOIP_ENABLED
+
+# Bootanimation
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+# Custom LUN File Path
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
 # cat /proc/emmc
 #dev:        size     erasesize name
 #mmcblk0p31: 000ffa00 00000200 "misc"
@@ -69,36 +90,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 1252770816
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_FLASH_BLOCK_SIZE := 262144
 
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Data
+BOARD_USE_NEW_LIBRIL_HTC := true
 
-# Fix building until we can figure v8 out
-#DYNAMIC_SHARED_LIBV8SO := false
-
-# Graphics / Video
-COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
-
-# Camera
-BOARD_HTC_3D_SUPPORT := true
-TARGET_HARDWARE_3D := true
-BOARD_HAVE_HTC_FFC := true
-
-# Wifi
-BOARD_LEGACY_NL80211_STA_EVENTS := true
-WIFI_DRIVER_MODULE_NAME          := bcmdhd
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
-
-# Bootanimation
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := false
-
-# Custom LUN File Path
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
-
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := shooteru
